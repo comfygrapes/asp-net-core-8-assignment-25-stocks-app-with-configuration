19cf5bae8baae1b81a46763eaf6ee2b805e90f7f
comfygrapes <76193542+comfygrapes@users.noreply.github.com>
2024-02-27 21:27:12 +0800

    Added trading options service injection

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,8 +1,10 @@
 using Services;
+using StocksApp.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddScoped<FinnhubService, FinnhubService>();
 
 var app = builder.Build();
